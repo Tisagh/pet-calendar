d700a296b2b7faeab954fc186b0e3a811e6cca08
Tisagh Chase <39168335+Tisagh@users.noreply.github.com>
2022-12-19 01:22:47 -0400

    installed django

--- petcalendar/pets/views.py
+++ petcalendar/pets/views.py
@@ -6,9 +6,9 @@ from datetime import datetime
 User = get_user_model()
 
 # views
-@login_required()
 def index(request, *args, **kwargs):
   template = 'pets/index.html'
   context = {
     'year': datetime.now().year,
   }
+  return render(request, template, context)
